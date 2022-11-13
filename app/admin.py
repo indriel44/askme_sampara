33a7703f716afb5faf830475c3a3e3ee170b1698
Oleg Sampara <lazers03@mail.ru>
2022-11-14 01:27:43 +0300

    DZ3 Models+migrations

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from app.models import User, Question, Answer, Tag, Like, Profile
 
-# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Tag)
+admin.site.register(Like)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 QUESTIONS = [
@@ -38,3 +39,54 @@ TAGS = [
         'title': f'Tag #{tag_id}',
     } for tag_id in range(3)
 ]
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=15)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(blank=True, null=True)
+
+
+class Like(models.Model):
+    number = models.IntegerField()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    models.ForeignKey(Profile, related_name='profile_related', on_delete=models.CASCADE)
+    like = models.OneToOneField(Like, on_delete=models.CASCADE)
+
+
+class QuestionManager(models.Manager):
+    def get_hot_questions(self):
+        return self.filter(status__exact='h')
+
+    def get_new_questions(self):
+        return self.objects.order_by('date')
+
+    def get_tagged_questions(self, tag):
+        return self.filter(tag__exact=tag)
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=60)
+    tag = models.ManyToManyField(Tag)
+    text = models.TextField()
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    like = models.OneToOneField(Like, on_delete=models.CASCADE)
+    models.ForeignKey(Profile, related_name='profile_related', on_delete=models.CASCADE)
+
+    HOT = 'h'
+    NORMAL = 'n'
+    STATUSES = [
+        (HOT, 'hot'),
+        (NORMAL, 'ok')
+    ]
+
+    status = models.CharField(max_length=1, choices=STATUSES)
+    date = models.DateTimeField(blank=True, null=True)
+
+    objects = QuestionManager
